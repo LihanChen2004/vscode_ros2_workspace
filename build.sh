e176140a0e911f845f9df84317b7654111fa3d6f
LihanChen2004 <lihan.chen@mondorobotics.com>
2025-10-27 08:07:45 +0000

    移除构建命令中的编译命令导出选项

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ BUILD_TYPE=RelWithDebInfo
 colcon build \
   --merge-install \
   --symlink-install \
-  --cmake-args "-DCMAKE_BUILD_TYPE=$BUILD_TYPE" "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON"
+  --cmake-args "-DCMAKE_BUILD_TYPE=$BUILD_TYPE"
